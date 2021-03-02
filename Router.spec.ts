fe5b16fb9c1dc02f4ce07c8f88e34b2c275d27b6
Florian Ebmeier <florian.ebmeier@payfunc.com>
2021-03-02 09:39:05 +0100

    Fix tests

--- Router.spec.ts
+++ Router.spec.ts
@@ -3,7 +3,7 @@ import * as cloudRouter from "./index"
 describe("Router", () => {
 	it("create router", () => {
 		const router = new cloudRouter.Router()
-		router.add("GET", "/test", async request => request.url)
+		router.add("GET", "/test", async (request: any) => request.url)
 		expect(router).toMatchObject({
 			origin: ["*"],
 			routes: [
