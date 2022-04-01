9a5dfc31dcbb0e5f4967f344aefe490463e43585
Christoffer Johansson <christoffer@issuefab.com>
2022-04-01 17:47:57 +0200

    Allowed headers modifiable.

--- Router.spec.ts
+++ Router.spec.ts
@@ -29,4 +29,18 @@ describe("Router", () => {
 			body: "/api/test",
 		})
 	})
+	it("custom allow headers on options", async () => {
+		const router = new cloudRouter.Router()
+		router.allowedHeaders.push("X-Auth-Token")
+		router.add("POST", "/test", async (request: http.Request) => request.url.pathname)
+		expect(
+			await router.handle(http.Request.create({ method: "OPTIONS", url: "https://example.com/test" }), {})
+		).toMatchObject({
+			header: {
+				accessControlAllowMethods: ["POST"],
+				accessControlAllowOrigin: "*",
+				accessControlAllowHeaders: ["Content-Type", "Authorization", "X-Auth-Token"],
+			},
+		})
+	})
 })
