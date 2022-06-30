3efecf1ba862854f139c5c549dffa8281b5bdd67
Magnus Gustafsson <magnus@intergiro.com>
2022-06-30 12:53:58 +0200

    Match wildcard and repeating pathnames with minimum 1 (#16)
    * match repeating group with 1+ and wildcard
    
    * Urlpattern (#17)
    
    * Switched to use URLPattern.
    
    * Removed deprecated tslint configuration.
    
    * Improved code.
    
    * Updated configuration and dependencies.
    
    Co-authored-by: Simon Mika <simonmika@users.noreply.github.com>

--- Route.spec.ts
+++ Route.spec.ts
@@ -1,11 +1,11 @@
-import * as cloudRouter from "./index"
 import * as http from "cloudly-http"
+import * as cloudRouter from "./index"
 
 describe("Route", () => {
 	it("create", () => {
 		const route = cloudRouter.Route.create("GET", "/test", async (request: any) => request.url)
 		expect(route).toMatchObject({
-			expression: /\/test/,
+			pattern: {},
 			methods: ["GET"],
 		})
 	})
@@ -20,9 +20,31 @@ describe("Route", () => {
 	it("match alternatePrefix", () => {
 		const route = cloudRouter.Route.create("GET", "/test", async (request: any) => request.url)
 		const request = http.Request.create("https://example.com/api/test")
-		expect(route.match(request, "api")).toEqual({
+		expect(route.match(request, "/api")).toEqual({
 			...request,
 			parameter: {},
+		})
+	})
+	it("match repeating group with a minimum of one", () => {
+		const route = cloudRouter.Route.create("GET", "/test/:id/:trailingPath+", async (request: any) => request.url)
+		const request = http.Request.create("https://example.com/test/someid/path/to/something?testquery=value")
+		expect(route.match(request)).toEqual({
+			...request,
+			parameter: { id: "someid", trailingPath: "path/to/something" },
+			search: { testquery: "value" },
+		})
+		const request2 = http.Request.create("https://example.com/test/someid/?testquery=value")
+		expect(route.match(request2)).toEqual(undefined)
+		const request3 = http.Request.create("https://example.com/test/someid?testquery=value")
+		expect(route.match(request3)).toEqual(undefined)
+	})
+	it("match wildcard", () => {
+		const route = cloudRouter.Route.create("GET", "/test/:id/*", async (request: any) => request.url)
+		const request = http.Request.create("https://example.com/test/someid/path/to/something?testquery=value")
+		expect(route.match(request)).toEqual({
+			...request,
+			parameter: { "0": "path/to/something", id: "someid" },
+			search: { testquery: "value" },
 		})
 	})
 })
