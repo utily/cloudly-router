27a5a6e16a4bf4f1b3007d12fbdeee419af3c97f
Simon Mika <simon@mika.se>
2023-03-15 18:49:27 +0100

    Enabled tests.

--- Route.spec.ts
+++ Route.spec.ts
@@ -18,34 +18,34 @@ describe("Route", () => {
 			parameter: {},
 		})
 	})
-	// it("match alternatePrefix", () => {
-	// 	const route = Route.create("GET", "/test", async (request: any) => request.url, undefined)
-	// 	const request = http.Request.create("https://example.com/api/test")
-	// 	expect(route.match(request, "/api")).toEqual({
-	// 		...request,
-	// 		parameter: {},
-	// 	})
-	// })
-	// it("match repeating group with a minimum of one", () => {
-	// 	const route = Route.create("GET", "/test/:id/:trailingPath+", async (request: any) => request.url, undefined)
-	// 	const request = http.Request.create("https://example.com/test/someid/path/to/something?testquery=value")
-	// 	expect(route.match(request)).toEqual({
-	// 		...request,
-	// 		parameter: { id: "someid", trailingPath: "path/to/something" },
-	// 		search: { testquery: "value" },
-	// 	})
-	// 	const request2 = http.Request.create("https://example.com/test/someid/?testquery=value")
-	// 	expect(route.match(request2)).toEqual(undefined)
-	// 	const request3 = http.Request.create("https://example.com/test/someid?testquery=value")
-	// 	expect(route.match(request3)).toEqual(undefined)
-	// })
-	// it("match wildcard", () => {
-	// 	const route = Route.create("GET", "/test/:id/*", async (request: any) => request.url, undefined)
-	// 	const request = http.Request.create("https://example.com/test/someid/path/to/something?testquery=value")
-	// 	expect(route.match(request)).toEqual({
-	// 		...request,
-	// 		parameter: { "0": "path/to/something", id: "someid" },
-	// 		search: { testquery: "value" },
-	// 	})
-	// })
+	it("match alternatePrefix", () => {
+		const route = Route.create("GET", "/test", async (request: any) => request.url, undefined)
+		const request = http.Request.create("https://example.com/api/test")
+		expect(route.match(request, "/api")).toEqual({
+			...request,
+			parameter: {},
+		})
+	})
+	it("match repeating group with a minimum of one", () => {
+		const route = Route.create("GET", "/test/:id/:trailingPath+", async (request: any) => request.url, undefined)
+		const request = http.Request.create("https://example.com/test/someid/path/to/something?testquery=value")
+		expect(route.match(request)).toEqual({
+			...request,
+			parameter: { id: "someid", trailingPath: "path/to/something" },
+			search: { testquery: "value" },
+		})
+		const request2 = http.Request.create("https://example.com/test/someid/?testquery=value")
+		expect(route.match(request2)).toEqual(undefined)
+		const request3 = http.Request.create("https://example.com/test/someid?testquery=value")
+		expect(route.match(request3)).toEqual(undefined)
+	})
+	it("match wildcard", () => {
+		const route = Route.create("GET", "/test/:id/*", async (request: any) => request.url, undefined)
+		const request = http.Request.create("https://example.com/test/someid/path/to/something?testquery=value")
+		expect(route.match(request)).toEqual({
+			...request,
+			parameter: { "0": "path/to/something", id: "someid" },
+			search: { testquery: "value" },
+		})
+	})
 })
