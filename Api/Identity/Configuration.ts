86069fde30f122dd234478200c6e3effc9f42d98
Simon Mika <simon@mika.se>
2025-03-13 10:02:22 +0100

    Made context available when verifying identity.

--- Api/Identity/Configuration.ts
+++ Api/Identity/Configuration.ts
@@ -1,12 +1,13 @@
+import { gracely } from "gracely"
 import { http } from "cloudly-http"
 import { isly } from "isly"
 import type { Identity } from "."
 
-export interface Configuration<I extends Identity | undefined, Token = string> {
+export interface Configuration<I extends Identity | undefined, Context = unknown, Token = string> {
 	name: string
 	description: string
 	header: { name: keyof http.Request.Header; type: isly.Type<Token> }
 	type: isly.Type<I>
-	authenticate: (token: Token) => Promise<I | undefined>
+	authenticate: (token: Token, context: Context) => Promise<I | gracely.Error | undefined>
 }
 export namespace Configuration {}
--- Api/Identity/index.ts
+++ Api/Identity/index.ts
@@ -1,3 +1,4 @@
+import { gracely } from "gracely"
 import { http } from "cloudly-http"
 import { isly } from "isly"
 import { Configuration as _Configuration } from "./Configuration"
@@ -7,15 +8,17 @@ export interface Identity {}
 
 export namespace Identity {
 	export import Configuration = _Configuration
-	export async function verify<I extends Identity | undefined>(
+	export async function verify<I extends Identity | undefined, Context = unknown>(
 		configurations: Configuration<I>[],
-		header: http.Request.Header
-	): Promise<I | isly.Flaw[]> {
+		header: http.Request.Header,
+		context: Context
+	): Promise<I | gracely.Error | isly.Flaw[]> {
 		const configuration = configurations.find(configuration =>
 			configuration.header.type.is(header[configuration.header.name])
 		)
 		const token = configuration?.header.type.get(header[configuration.header.name])
-		const result: I | undefined = token != undefined ? await configuration?.authenticate(token) : undefined
+		const result: I | gracely.Error | undefined =
+			token != undefined ? await configuration?.authenticate(token, context) : undefined
 		return result != undefined
 			? result
 			: (configuration
